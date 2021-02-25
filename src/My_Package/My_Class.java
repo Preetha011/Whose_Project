c17dea67e9923cb6540a7846ae53388519e690c8
kodee <preethukrish01@gmail.com>
2021-02-25 18:06:31 +0530

    Hello

---
+++ src/My_Package/My_Class.java
@@ -0,0 +1,10 @@
+package My_Package;
+
+public class My_Class {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ src/My_Package/Practice_me.java
@@ -0,0 +1,10 @@
+package My_Package;
+
+public class Practice_me {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
